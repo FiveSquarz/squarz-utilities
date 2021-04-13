8d36478ca48fad4ae37a38e4ee18d35d397e3603
FiveSquarz <chuanhaixu@gmail.com>
2021-04-13 18:50:07 -0400

    first stuffs

---
+++ Runtime/TestUtilities.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyUtilities {
+    public static class TestUtilities {
+        public static Vector3 Test() {
+            return new Vector3(3, 5, 6);
+        }
+    }
+}
