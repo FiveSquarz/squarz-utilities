16171a46ead6f80046da931316b19e4e404b8b5d
FiveSquarz <chuanhaixu@gmail.com>
2021-04-16 19:09:26 -0400

    Renamed to squarz-utilities

--- Runtime/TestUtilities.cs
+++ Runtime/TestUtilities.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace MyUtilities {
+namespace SquarzUtilities {
     public static class TestUtilities {
         public static Vector3 Test() {
             return new Vector3(3, 5, 6);
